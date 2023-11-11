7ff37df276196bd354e813d139fbd2af99f83945
Alexander Menzel <alexander@menzel.dk>
2023-11-11 18:03:46 +0100

    Fixes drag n drop flicker issue + improves supabase query

--- components/DeckList.tsx
+++ components/DeckList.tsx
@@ -1,7 +1,8 @@
+import classNames from 'classnames'
 import clsx from 'clsx'
 import { PlusCircle } from 'lucide-react'
-import { useContext, useEffect, useMemo, useState } from 'react'
-import { Draggable, Droppable } from 'react-beautiful-dnd'
+import { useContext, useEffect } from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 import { AppContext } from '../context/app'
 import { template } from '../lib/utils'
 import Deck from '../models/Deck'
@@ -9,36 +10,42 @@ import DeckPreview from './DeckPreview'
 
 type Props = React.HTMLAttributes<HTMLDivElement>
 
+const reorder = (decks: Deck[], startIndex: number, endIndex: number) => {
+	const result = Array.from(decks)
+	const [removed] = result.splice(startIndex, 1)
+	result.splice(endIndex, 0, removed)
+
+	return result.map((deck, idx) => ({ ...deck, order: idx }))
+}
+
 const DeckList: React.FC<Props> = (props) => {
 	const { className } = props
 	const { state, actions } = useContext(AppContext)
-	const defaultDecksSort = useMemo(() => {
-		return state.decks.sort((deckA, deckB) => deckA.order - deckB.order)
-	}, [state.decks])
-	const [filteredDecks, setFilteredDecks] = useState<Deck[]>(defaultDecksSort)
+	//const [decks, setDecks] = useState<Deck[]>(state.decks)
+
 	const handleNewDeck = async () => {
 		if (!state.user) return
 
 		await actions.addDeck(
-			template.newDeck(
-				state.decks.length,
-				state.user.id,
-				state.activeTag ? state.activeTag : undefined
-			)
+			template.newDeck(state.decks.length, state.user.id)
 		)
 	}
 
 	useEffect(() => {
-		if (state.activeTag === null) {
-			setFilteredDecks(defaultDecksSort)
-		} else {
-			setFilteredDecks(
-				state.decks
-					.filter((deck) => deck.tag_id === state.activeTag?.id)
-					.sort((deckA, deckB) => deckA.order - deckB.order)
-			)
-		}
-	}, [state.decks, state.activeTag])
+		console.log('Decks updated', state.decks)
+	}, [state.decks])
+
+	const handleDragEnd = (result: any) => {
+		if (!result.destination) return
+		const { source, destination } = result
+		if (source.index === destination.index) return
+		const reorderedDecks = reorder(
+			state.decks,
+			source.index,
+			destination.index
+		)
+		actions.setDecks(reorderedDecks)
+	}
 
 	return (
 		<div
@@ -46,35 +53,45 @@ const DeckList: React.FC<Props> = (props) => {
 				'overflow-y-auto hide-scrollbar flex-grow flex relative flex-col items-center gap-y-2 w-full',
 				className
 			)}>
-			{filteredDecks.length > 0 ? (
-				<Droppable droppableId='droppable-1' type='PERSON'>
-					{(provided) => (
-						<div
-							className='w-full flex flex-col'
-							ref={provided.innerRef}
-							{...provided.droppableProps}>
-							{filteredDecks.map((deck, idx) => (
-								<Draggable
-									draggableId={`draggable-${idx}`}
-									key={idx}
-									index={idx}>
-									{(pr) => (
-										<div
-											ref={pr.innerRef}
-											{...pr.draggableProps}>
-											<DeckPreview
-												key={idx}
-												deck={deck}
-												handleProps={pr.dragHandleProps}
-											/>
-										</div>
-									)}
-								</Draggable>
-							))}
-							{provided.placeholder}
-						</div>
-					)}
-				</Droppable>
+			{state.decks.length > 0 ? (
+				<DragDropContext onDragEnd={handleDragEnd}>
+					<Droppable droppableId='droppable'>
+						{(provided) => (
+							<div
+								className='w-full flex flex-col'
+								ref={provided.innerRef}
+								{...provided.droppableProps}>
+								{state.decks.map((deck, idx) => (
+									<Draggable
+										draggableId={`draggable-${deck.id}`}
+										key={deck.id}
+										index={idx}>
+										{(pr) => (
+											<div
+												className={classNames({
+													hidden:
+														state.activeTag &&
+														deck.tag_id !==
+															state.activeTag.id,
+												})}
+												ref={pr.innerRef}
+												{...pr.draggableProps}>
+												<DeckPreview
+													key={idx}
+													deck={deck}
+													handleProps={
+														pr.dragHandleProps
+													}
+												/>
+											</div>
+										)}
+									</Draggable>
+								))}
+								{provided.placeholder}
+							</div>
+						)}
+					</Droppable>
+				</DragDropContext>
 			) : (
 				<p className='max-w-sm text-center'>
 					A card deck is a collection of cards used to practice a
